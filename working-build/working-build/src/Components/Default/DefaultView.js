21a6006a8041b97ea3a02ef8b06dc908d5b00c66
justprzy <jprzy435@mtroyal.ca>
2021-11-07 22:54:07 -0700

    Working sorts

--- working-build/working-build/src/Components/Default/DefaultView.js
+++ working-build/working-build/src/Components/Default/DefaultView.js
@@ -2,10 +2,12 @@ import React from "react";
 import { useContext } from "react";
 import FavoritesContext from "../../store/favorites-context";
 import SortPlays from "./SortPlays";
-
 import ListOfPlays from "./ListOfPlays";
 
 function DefaultView(props) {
+
+
+  /**
   const favContext = useContext(FavoritesContext);
   const playIsFav = favContext.playIsFavorite(props.plays.id);
 
@@ -17,19 +19,15 @@ function DefaultView(props) {
       favContext.addFavorite(props.plays.id);
     }
   }
-
+*/
   return (
     <div>
-      <SortPlays />
-
-      <button>Title</button>
-      <button>Year</button>
-      <ul>
-        {props.plays.map((plays) => {
-          return <ListOfPlays aPlay={plays} key={plays.id} />;
-        })}
-      </ul>
+      
+      <SortPlays plays={props.plays} key={props.plays.id}></SortPlays>
     </div>
   );
 }
 export default DefaultView;
+
+{/* <button>Title</button>
+      <button>Year</button> */}
