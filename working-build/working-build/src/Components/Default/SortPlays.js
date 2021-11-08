9fb0db68d6de8254afc850006f66df5b839a774c
Michael Sujatno <msuja061@mtroyal.ca>
2021-11-08 13:50:55 -0700

    work in-progress filter by years

--- working-build/working-build/src/Components/Default/SortPlays.js
+++ working-build/working-build/src/Components/Default/SortPlays.js
@@ -4,9 +4,10 @@ import React from "react";
 
 const SortPlays = (props) => {
   let list = props.plays;
+  let tempList = props.plays;
   const [howToSort, setHowToSort] = React.useState("title");
 
-  const handleChange = (event) =>{
+  const sortChange = (event) =>{
     if (event.target.value === "title") {
       setHowToSort("title");
     } else if (event.target.value === "year") {
@@ -32,9 +33,6 @@ const SortPlays = (props) => {
     console.log("Printing only dates: ");
     console.log(dateSort);
     list = dateSort;
-      
-    
-
   }
 
   const sortByGenre = () => {
@@ -46,11 +44,65 @@ const SortPlays = (props) => {
     list = genreSort;
   }
 
- 
+  const handleYearChange = (event) => {
+    console.log("Year changed...");
+    console.log(event.target.value);
+    console.log(event.target.name);
+    let startYear=0;
+    let endYear=0;
+    
+    if(event.target.name === "start" && event.target.value !== "") {
+      startYear = event.target.value;
+      console.log("startYear: " + startYear);
+    }else if(event.target.name === "end" && event.target.value !== ""){
+      endYear = event.target.value;
+      console.log("endYear: " + endYear);
+    }
+    
+    if(startYear !== 0 && endYear !== 0){
+      const betweenYears = props.plays.filter((a) => a.likelyDate >= startYear && a.likelyDate <= endYear);
+      list = betweenYears;
+      // tempList = betweenYears;
+      setHowToSort("between");
+
+    } 
+    else if(startYear !== 0 && endYear === 0){
+      const afterYear = props.plays.filter((a) => a.likelyDate >= startYear);
+      list = afterYear;
+      // tempList = afterYear;
+      setHowToSort("after");
+    }
+    else if(startYear === 0 && endYear !== 0){
+      const beforeYear = props.plays.filter((a) => a.likelyDate <= endYear);
+      list = beforeYear;
+      // tempList = beforeYear;
+      setHowToSort("before");
+    }
+    else{
+      list = props.plays;
+      // tempList = props.plays;
+    }
+
+    console.log("Printing filtered list: ");
+    // console.log(tempList);
+    console.log(list);
+
+    // return list.map( (plays) => <ListOfPlays aPlay={plays} key={plays.id} />);
+  }
+
+  const filterByYear = () => {
+    console.log("filter by year");
+    console.log(props.plays);
+    const yearFilter = props.plays.filter((play) => play.likelyDate.includes(props.filter));
+    console.log("Printing only years: ");
+    console.log(yearFilter);
+    list = yearFilter;
+  } 
 
   function genList() {
     if (howToSort === "title") {
       sortByTitle();
+      // filterByTitle();
       return list.map( (plays) => <ListOfPlays aPlay={plays} key={plays.id} />);
     } else if (howToSort === "year") {
       SortByDate();
@@ -58,16 +110,29 @@ const SortPlays = (props) => {
     } else if (howToSort === "genre") {
       sortByGenre();
       return list.map( (plays) => <ListOfPlays aPlay={plays} key={plays.id} />);
+    } else if (howToSort === "between") {
+      return list.map( (plays) => <ListOfPlays aPlay={plays} key={plays.id} />);
+      return tempList.map( (plays) => <ListOfPlays aPlay={plays} key={plays.id} />);
+    } else if (howToSort === "after") {
+      return list.map( (plays) => <ListOfPlays aPlay={plays} key={plays.id} />);
+    } else if (howToSort === "before") {
+      return list.map( (plays) => <ListOfPlays aPlay={plays} key={plays.id} />);
     } else {
       return list.map( (plays) => <ListOfPlays aPlay={plays} key={plays.id} />);
     }
+  }
+
+  function genList2() {
+    console.log("genList2: ");
+    console.log(tempList);
+    return tempList.map( (plays) => <ListOfPlays aPlay={plays} key={plays.id} />);
   }
 
     return (
       <section>
         <label>
           Sort by:
-          <form onChange={handleChange}>
+          <form onChange={sortChange}>
             <select>
               <option value="select">Select</option>
               <option value="title">Title</option>
@@ -76,9 +141,19 @@ const SortPlays = (props) => {
             </select>
           </form>
         </label>
+         <label>
+           Filter by years:
+           <form onChange={handleYearChange}>
+             <input type="text" name="start" value={props.filter} placeholder="Starting Date" />
+             <input type="text" name="end" value={props.filter} placeholder="Ending Date" />
+          </form>
+        </label>
         <ul>
           {genList()}
         </ul>
+        {/* <ul>
+          {genList2()}
+        </ul> */}
       </section>
     );
 }
